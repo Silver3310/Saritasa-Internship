b462bb3035ddda65a7c8aa8a1b19fd87710cdf7d
Alexey <supersilver3310@gmail.com>
2018-07-26 01:14:09 +0700

    solve Counter from CSV
    The first task was edited to meet the requirements so that it works
    for any data and needs csv file as an input

--- counter_from_csv.py
+++ counter_from_csv.py
@@ -1,4 +1,5 @@
 import csv, argparse
+from collections import defaultdict
 
 
 def name_via_parsing():
@@ -14,37 +15,56 @@ def name_via_parsing():
 
 
 def dict_to_count(file_name):
+    # a list that will be containing our records
     records = list()
-    with open('input.csv') as csvfile:
+    with open(file_name) as csvfile:
+        # here we tell that we want to open it as a dict
         reader = csv.DictReader(csvfile)
         for row in reader:
             records.append(row)
     return records
 
 
-def counter(records_list, key, value):
-    count = 0
-    for each in records_list:
-        if each[key] == value:
-            count += 1
-    return dict(key=key, value=value, count=count)
-
-
 def list_counter(records):
-    c_list = list()
-    result = dict()
-    keys = records[0].keys()
+    # this function consists of two parts
+
+    # in this part we count the same values of keys
+
+    # we use defaultdict(dict) as it uses the default value (which is dict, the empty dict) if it
+    # encounters an unknown value
+    result = defaultdict(dict)
+    # for each record
     for each in records:
-        for k in keys:
-            if k not in result.keys():
-                result[k] = list()
-            result[k].append(each[k])
-            
+        # for each key
+        for k in records[0].keys():
+            try:
+                # each[k] - the value of the particular key, we're about to increment it
+                result[k][each[k]] += 1
+            except KeyError:
+                # or to create it :)
+                result[k][each[k]] = 1
+    # as a result we'll get sth like {{success: {True: 5000, False: 5000}}, {{name: {alex: 1, john: 3...
+
+    # creating the final list
+    # now we need to meet the requirements of how the counter list should look like
+    # let's create an empty list list
+    c_list = list()
+    for key, v in result.items():
+        # e.g. key=success, v = (True: 5000, False: 5000)
+        for value in v.items():
+            # e.g. value[0] = True, value[1] = 5000
+            c_list.append(dict(key=key, value=value[0], counter=value[1]))
+            # so we have sth like {key=success, value=True, counter=5000}
+    print(c_list)
+
 
 def main():
-    # get the name of the input file
+    # get the name of the input file through parsing ( we must specify
+    # the input file in the command line)
     file_name = name_via_parsing()
+    # make the list of records that are contained in the input file
     records = dict_to_count(file_name)
+    # make a counter list based on the records
     list_counter(records)
 
 
---
+++ init_csv.py
@@ -0,0 +1,27 @@
+from faker import Faker
+import csv
+
+
+def init_csv_file(source):
+    with open('input.csv', 'w') as csv_file:
+        # here we specify the field names of our records
+        field_names = ['name', 'success', 'email']
+        writer = csv.DictWriter(csv_file, fieldnames=field_names)
+        # the first row will be the names of the fields in order to make it clear
+        # what the following rows mean
+        writer.writerow({'name': 'name', 'success': 'success', 'email': 'email'})
+        # the following rows
+        for i in range(10000):
+            writer.writerow({'name': source.name(), 'success': source.boolean(), 'email': source.email()})
+
+
+def main():
+    fake = Faker()
+    init_csv_file(fake)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
