cdf7113532bb519d7296b3b5981e4cfe09a5c95b
Silver3310 <epifanov998@mail.ru>
2018-07-26 15:22:10 +0700

    Add docstrings to Day_1/counter.py

--- Day_1/counter.py
+++ Day_1/counter.py
@@ -1,9 +1,31 @@
+# -*- coding: utf-8 -*-
+"""A module that counts the values in dictionary by keys
+
+This module demonstrates a counter of values in dictionary by keys
+based on the list of records which is created randomly
+
+Example:
+    As an input is static you'll always see the same output
+    The first row is a list of records itself, the second row
+    is a list of counters::
+
+        [{'id': 0, 'success': False, 'name': 'Mary'}, {'id': 1, ...
+        [{'key': 'success', 'value': True, 'count': 476}, {'key': ....
+
+"""
+
 from random import choice
 
-names_list = ['Laura', 'Alex', 'Anna', 'Kate', 'John', 'Jake', 'Peter', 'Mary']
+names_list = ['Lucy', 'Alex', 'Anna', 'Kate', 'John', 'Jake', 'Peter', 'Mary']
 
 
 def init_records(record_list):
+    """This function fills the empty record list with random values
+
+    Args:
+        record_list (list): an empty list
+
+    """
     for i in range(1000):
         record_list.append(dict(id=i,
                                 success=choice([True, False]),
@@ -11,6 +33,15 @@ def init_records(record_list):
 
 
 def counter(records_list, key, value):
+    """The function for counting values by keys
+
+    This function takes the given value and key and starts
+    counting records that have the key with a given value
+
+    Returns:
+        a counter dict, e.g.{'key': 'success', 'value': True, 'count': 476}
+
+    """
     count = 0
     for each in records_list:
         if each[key] == value:
@@ -19,6 +50,12 @@ def counter(records_list, key, value):
 
 
 def counters_list(records_list):
+    """This function applies counter function to all record_list items
+
+    Returns:
+        a list of counters
+
+    """
     c_list = list()
     c_list.append(counter(records_list, 'success', True))
     c_list.append(counter(records_list, 'success', False))
